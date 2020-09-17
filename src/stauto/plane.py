5ebfe2d50c7302a312a4915cd88ae83f7cca1cb8
Max Muir <maxwell.c.muir@accenture.com>
2020-09-17 18:39:51 -0400

    Mathmatically correct version that calculates all odds of seat availability given parameters. Still gives the same answer.

--- src/stauto/plane.py
+++ src/stauto/plane.py
@@ -1,2 +1,16 @@
 def odds_of_last_passenger_getting_correct_seat(total_seats):
     return total_seats if total_seats < 2 else .5
+
+
+def odds_that_seat_is_available_for_last(total_seats):
+    if total_seats < 2:
+        return total_seats
+    numerator = total_seats - 1
+    denominator = total_seats
+    for _ in range(2, total_seats):
+        numerator -= 1
+        denominator -= 1
+    return 1 - numerator/denominator
+
+
+
--- tests/test_plane.py
+++ tests/test_plane.py
@@ -1,4 +1,5 @@
-from stauto.plane import odds_of_last_passenger_getting_correct_seat
+from stauto.plane import odds_of_last_passenger_getting_correct_seat, \
+    odds_that_seat_is_available_for_last
 
 
 def test_odds_checker_returns_zero_if_plane_has_zero_seats():
@@ -12,3 +13,11 @@ def test_odds_checker_returns_one_if_plane_has_one_seat():
 def test_odds_checker_returns_half_if_plane_has_more_seats():
     assert odds_of_last_passenger_getting_correct_seat(2) == .5
     assert odds_of_last_passenger_getting_correct_seat(100) == .5
+
+
+def test_odds_that_seat_is_taken_for_first_passenger():
+    assert odds_that_seat_is_available_for_last(1) == 1
+
+
+def test_odds_that_seat_is_taken_for_second_passenger():
+    assert odds_that_seat_is_available_for_last(100) == .5
